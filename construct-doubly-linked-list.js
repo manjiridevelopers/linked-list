5b1d65af361a238a0feeecc881873830996be5ae
unknown <manjiri.developers@gmail.com>
2023-07-01 23:51:55 +0530

    singly and doubly linked list

---
+++ construct-doubly-linked-list.js
@@ -0,0 +1,148 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  push(element) {
+    let node = new Node(element);
+
+    if (this.head == null && this.tail == null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      let lastNode = this.tail;
+
+      lastNode.next = node;
+      node.prev = lastNode;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  unshift(element) {
+    let node = new Node(element);
+
+    if (this.head == null && this.tail == null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      let firstNode = this.head;
+
+      node.next = firstNode;
+      firstNode.prev = node;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  pop() {
+    if (this.size <= 0) return "No elements in the linked list";
+
+    if (this.size == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let lastNode = this.tail;
+      let prevOfLastNode = lastNode.prev;
+      prevOfLastNode.next = null;
+      lastNode.prev = null;
+      this.tail = prevOfLastNode;
+    }
+
+    this.size--;
+  }
+
+  shift() {
+    if (this.size <= 0) return "No elements in the linked list";
+
+    if (this.size == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let firstNode = this.head;
+      this.head = firstNode.next;
+      this.head.prev = null;
+    }
+
+    this.size--;
+  }
+
+  insertAt(index, element) {
+    if (index < 0 && index >= this.size) return "Invalid input!";
+    let position = 0;
+    let current = this.head;
+    let node = new Node(element);
+
+    if (index == 0) return this.unshift(element);
+    if (index == this.size - 1) return this.push(element);
+
+    while (current.next !== null) {
+      if (position == index) {
+        let before = current.prev;
+        let after = current;
+        after.prev = node;
+        node.next = after;
+        node.prev = before;
+        before.next = node;
+        break;
+      } else {
+        current = current.next;
+        position++;
+      }
+    }
+    this.size++;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.size - 1) return "Invalid input!";
+    let position = 0;
+    let current = this.head;
+
+    if (index == 0) return this.shift();
+    if (index == this.size - 1) return this.pop();
+
+    while (current.next !== null) {
+      if (position == index) {
+        let before = current.prev;
+        let after = current.next;
+
+        current.next = null;
+        current.prev = null;
+
+        before.next = after;
+        after.prev = before;
+        break;
+      } else {
+        current = current.next;
+        position++;
+      }
+    }
+
+    this.size--;
+  }
+}
+
+let dl1 = new DoublyLinkedList();
+dl1.push(1);
+dl1.push(2);
+dl1.push(3);
+dl1.push(4);
+dl1.push(5);
+dl1.push(6);
+dl1.unshift(0);
+dl1.pop();
+dl1.shift();
+dl1.insertAt(3, 4.1);
+dl1.removeAt(2);
+
+console.log(dl1);
---
+++ construct-linked-list.js
@@ -0,0 +1,123 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(element) {
+    let node = new Node(element);
+
+    let current = this.head;
+
+    if (current == null) this.head = node;
+    else {
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  insertAt(element, index) {
+    if (index < 0 || index > this.size) {
+      return "Invalid Position!";
+    }
+
+    let position = 0;
+    let prev = null;
+    let current = this.head;
+    let node = new Node(element);
+
+    if (index == 0) {
+      node.next = current;
+      this.head = node;
+    } else {
+      while (position < index) {
+        prev = current;
+        current = current.next;
+        position++;
+      }
+      prev.next = node;
+      node.next = current;
+    }
+
+    this.size++;
+  }
+
+  removeFrom(index) {
+    if (index < 0 || index > this.size) {
+      return "Invalid index!";
+    }
+
+    let position = 0;
+    let prev = null;
+    let current = this.head;
+
+    if (index == 0) {
+      this.head = this.head.next;
+    } else {
+      while (position < index) {
+        prev = current;
+        current = current.next;
+        position++;
+      }
+      prev.next = current.next;
+    }
+
+    this.size--;
+
+    return current.element;
+  }
+
+  removeElement(element) {
+    let prev = null;
+    let current = this.head;
+    let position = 0;
+
+    while (current.next !== null) {
+      if (current.element == element) {
+        if (prev == null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+        this.size--;
+        return position;
+      }
+      position++;
+      prev = current;
+      current = current.next;
+    }
+    return -1;
+  }
+}
+
+let l1 = new linkedList();
+l1.add(5);
+l1.add(7);
+l1.add(9);
+l1.add(10);
+
+l1.insertAt(8, 2);
+
+const removedElement = l1.removeFrom(2);
+console.log("Removed element:", removedElement);
+
+l1.insertAt(8, 2);
+
+const removedIdx = l1.removeElement(8);
+console.log("Removed Idx:", removedIdx);
+
+console.log("Size of linked list:", l1.size);
+
+console.log(l1);
